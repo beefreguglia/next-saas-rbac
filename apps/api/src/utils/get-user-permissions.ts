42e4e1c4fc0f221c47496c34c7d3f6876829907f
beefreguglia <beefreguglia@gmail.com>
2024-04-29 19:57:02 -0300

    refact: create update and shudown routes

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,13 @@
+import { defineAbilityFor, type Role, userSchema } from '@beef-saas/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbilityFor(authUser)
+  ability.can = ability.can.bind(ability)
+  ability.cannot = ability.cannot.bind(ability)
+  return ability
+}
