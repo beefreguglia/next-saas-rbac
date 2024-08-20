24eb8dfe9f2345e0bf19a87b8b3ab4d881f2f28a
Bernardo <beefreguglia@gmail.com>
2024-08-20 20:09:55 -0300

    feature: create subject types

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,6 +3,11 @@ import { defineAbilityFor } from '@saas/auth'
 const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanInviteProject = ability.can('manage', 'Project')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
 
-console.log(userCanInviteSomeoneElse, userCanDeleteOtherUsers)
+console.log(
+  userCanInviteSomeoneElse,
+  userCanDeleteOtherUsers,
+  userCanInviteProject,
+)
