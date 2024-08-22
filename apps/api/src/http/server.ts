ec171df7bf02dea0df22a02fe5e795072792d4fd
Bernardo <beefreguglia@gmail.com>
2024-08-22 19:42:36 -0300

    feature: create request password recovery route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,7 @@ import { errorHandler } from './error-handler'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecovery } from './routes/auth/request-password-recovery'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -52,6 +53,7 @@ app.setErrorHandler(errorHandler)
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
+app.register(requestPasswordRecovery)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
