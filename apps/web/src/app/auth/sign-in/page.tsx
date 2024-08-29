ba68d8cc95ded08cae50f9bab72eba3a0737f7ba
Bernardo <beefreguglia@gmail.com>
2024-08-29 13:13:40 -0300

    feature: create sign i page

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input type="email" name="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="email">Password</Label>
+        <Input type="password" name="password" id="password" />
+        <Link
+          href="/auth/forgot-password"
+          className="block w-full text-end text-xs font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+      <Button type="submit" className="w-full">
+        Sign in with e-mail
+      </Button>
+      <Separator orientation="horizontal" />
+      <Button variant="outline" className="w-full">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign in with Github
+      </Button>
+    </form>
+  )
+}
