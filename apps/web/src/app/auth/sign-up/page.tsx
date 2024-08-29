bfc72396d286f30ca0766f3a7f2be0036a88fc24
Bernardo <beefreguglia@gmail.com>
2024-08-29 20:16:07 -0300

    feature: create sign up and forgot password pages

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -7,9 +7,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Nome</Label>
+        <Input type="text" name="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input type="email" name="email" id="email" />
@@ -17,23 +21,25 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input type="password" name="password" id="password" />
-        <Link
-          href="/auth/forgot-password"
-          className="block w-full text-end text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          type="password"
+          name="password_confirmation"
+          id="password_confirmation"
+        />
       </div>
       <Button type="submit" className="w-full">
-        Sign in with e-mail
+        Create account
       </Button>
       <Button variant="link" className="w-full" asChild size="sm">
-        <Link href="/auth/sign-up">Create a new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
       <Separator orientation="horizontal" />
       <Button variant="outline" className="w-full">
         <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body className="dark">{children}</body>
     </html>
   )
