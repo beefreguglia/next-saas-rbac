b9fb0184eeab03d346fe82f8b2794da4a5be9b20
Bernardo <beefreguglia@gmail.com>
2024-09-03 16:34:48 -0300

    feature: obtain user permissions

---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,20 @@
+import { Roles } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetMembershipResponse {
+  membership: {
+    id: string
+    role: Roles
+    organizationId: string
+    userId: string
+  }
+}
+
+export async function getMembership(org: string) {
+  const result = await api
+    .get(`organizations/${org}/membership`)
+    .json<GetMembershipResponse>()
+
+  return result
+}
