970aa5368ab5dc55ed5805dc3977fd3c6eb4d064
Bernardo <beefreguglia@gmail.com>
2024-09-20 16:01:55 -0300

    fix: use env to set cliend id

--- apps/web/src/app/auth/actions.tsx
+++ apps/web/src/app/auth/actions.tsx
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation'
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'http://github.com')
 
-  githubSignInURL.searchParams.set('client_id', 'c7c73e1db925269730c4')
+  githubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignInURL.searchParams.set(
     'redirect_uri',
     env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
